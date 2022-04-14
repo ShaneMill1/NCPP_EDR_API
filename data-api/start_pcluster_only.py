d1977838170a6d33d7c2403f29542e88e6d3e8f8
AWS ParallelCluster user <ec2-user@ip-172-31-44-45.us-west-2.compute.internal>
2022-04-14 16:42:55 +0000

    adding a test to run pcluster only without web service on headnode

---
+++ data-api/start_pcluster_only.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+from dask.distributed import Client, LocalCluster
+from dask_jobqueue import SLURMCluster
+import os
+import time 
+
+
+
+while True:
+    cluster=SLURMCluster(header_skip=['--mem'],cores=2,memory="4GB",death_timeout=200,scheduler_options={'host': '0.0.0.0:5000'})
+    cluster.adapt(minimum_jobs=6,maximum_jobs=50)
+    os.environ['tcp_scheculer']=cluster.scheduler_address
+
+    os.environ['EDR_CONFIG']="./EDR/config/config_pcluster.yml"
+    time.sleep(1)
+#subprocess.run("/home/ec2-user/miniconda3/envs/env/bin/gunicorn -w 6 -b 0.0.0.0:5400 EDR.edr_point_server:app --limit-request-line 100000 --timeout 960",shell=True,check=True)
+
